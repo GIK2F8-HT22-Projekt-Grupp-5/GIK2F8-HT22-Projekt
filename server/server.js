1eb33489d5e8bdd1eed2e280d1defbc364ef125f
c0mboy <90767788+c0mboy@users.noreply.github.com>
2022-12-21 10:30:45 +0100

    Uppdatera

--- server/server.js
+++ server/server.js
@@ -1,35 +1,42 @@
-const { error } = require('console');
-const { json } = require('express');
-const express = require('express');
+const cardLocale = `enUS`;
+const cardResolution = `512x`;
+// let cardID = ``; //Behöver få id dynamiskt senare
+const cardExt = `png`;
+const cardUrl = `https://art.hearthstonejson.com/v1/render/latest/${cardLocale}/${cardResolution}/${cardID}.${cardExt}`;
+
+/* https://art.hearthstonejson.com/v1/render/latest/enUS/512x/AT_003.png */
+/* https://art.hearthstonejson.com/v1/render/latest/{LOCALE}/{RESOLUTION}/{CARD_ID}.{EXT} */
+
+const { error } = require("console");
+const { json } = require("express");
+const express = require("express");
 const app = express();
-const fs = require('fs/promises');
+const fs = require("fs/promises");
 
 const PORT = 5000;
-app.use(express.json())
-.use(express.urlencoded({extended:false}))
-.use((req, res, next) => {
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "*");
 
     next();
+  });
+
+app.get("/tasks", async (req, res) => {
+  try {
+    /*const task = await fs.readFile("./tasks.json");*/
+    res.status(425).send(JSON.parse("Hejhopp"));
+  } catch (error) {
+    res.status(500).send({ error: error.stack });
+  }
 });
 
-app.get('/tasks', async (req, res) => {
-    try{
-        /*const task = await fs.readFile("./tasks.json");*/
-        res.status(425).send(JSON.parse("Hejhopp"));
-    }
-    catch(error)
-    {
-        res.status(500).send({error: error.stack});
-    }
-
-});
-
-app.post("/tasks", async(req,res) =>{
-    try {
-        /*const task = req.body;
+app.post("/tasks", async (req, res) => {
+  try {
+    /*const task = req.body;
         const listBuffer = await fs.readFile("./tasks.json");
         const currentTasks = JSON.parse(listBuffer);
         let nextTaskId = 1;
@@ -40,23 +47,22 @@ app.post("/tasks", async(req,res) =>{
             nextTaskId++;
         }
         */
-        /*const nextTask = {id: nextTaskId, ...task};
+    /*const nextTask = {id: nextTaskId, ...task};
         const taskList = currentTasks ? [...currentTasks, nextTask] : [nextTask];*/
 
-        /*await fs.writeFile('./tasks.json',JSON.stringify(taskList));*/
-        
-        res.send("Hej hopp");
-    } 
-    catch (error) {
-        res.status(500).send({error: error.stack});
-    }
+    /*await fs.writeFile('./tasks.json',JSON.stringify(taskList));*/
+
+    res.send("Hej hopp");
+  } catch (error) {
+    res.status(500).send({ error: error.stack });
+  }
 });
 
-app.delete("/tasks/:id", async(req, res) =>{
-    /*localhost:5000/task/id*/
-   /* const id = req.params.id;*/
-    try {
-        /*const listBuffer = await fs.readFile("./tasks.json");
+app.delete("/tasks/:id", async (req, res) => {
+  /*localhost:5000/task/id*/
+  /* const id = req.params.id;*/
+  try {
+    /*const listBuffer = await fs.readFile("./tasks.json");
         const currentTasks = JSON.parse(listBuffer);
         if (currentTasks.length > 0) {
             await fs.writeFile("./tasks.json",JSON.stringify(currentTasks.filter(task => task.id != id )));
@@ -65,10 +71,9 @@ app.delete("/tasks/:id", async(req, res) =>{
         else{
             res.status(404).send({error: 'Ingen uppgift att ta bort'});
         }*/
-    } 
-    catch (error) {
-        res.status(500).send({error: error.stack});
-    }
+  } catch (error) {
+    res.status(500).send({ error: error.stack });
+  }
 });
 
-app.listen(PORT, () => console.log('Server running on http://localhost:5000'));
+app.listen(PORT, () => console.log("Server running on http://localhost:5000"));
