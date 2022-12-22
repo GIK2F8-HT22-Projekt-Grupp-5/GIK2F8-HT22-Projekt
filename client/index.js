3882b018f4e920f2d50d45d69a69e7f4d7a169dc
c0mboy <90767788+c0mboy@users.noreply.github.com>
2022-12-22 11:18:26 +0100

    Grid index.html

--- client/index.js
+++ client/index.js
@@ -9,4 +9,18 @@ function renderMain() {
   mainElement.innerHTML = ``;
   mainElement.insertAdjacentHTML("beforeend", getMain());
 }
+
+function test() {
+  // Requiring fs module in which
+  // readFile function is defined.
+  const fs = require("fs");
+  fs.readFile("Input.txt", (err, data) => {
+    if (err) throw err;
+    let testDat = JSON.parse(data);
+    
+    if (testDat.race ) 
+
+
+  });
+}
 //renderMain();
